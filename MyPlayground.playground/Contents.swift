3822e38a348f400e14bb075fbc234dd90ca50478
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2023-01-07 05:15:04 +0530

    Add files via upload

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let first = "Steve" //immutable variable
+var last = "Jobs"   //mutable Variable
+var new = first.uppercased();//Make Upper casee
+var new2 = new.lowercased()//lower case
+
+print(first + " " + last) //print
+
+var apple = ["iPhone", "MacBook", "iWatch"] //create Array
+apple.append("AirPods")//Add new element to array
+
+let oldArray = NSMutableArray( //NSMutuable array
+    array: ["MacOS","iOS"]
+)
+
+ 
---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+let i = 999
+let mac = 1000
+
+//IF condition
+if i < mac {
+    print("Mac")
+}
+else if mac<i {
+    print("i")
+}
+else{
+    print("nothing")
+}
+
+
+let num: Int //declair int variable
+var name:String //declair string variable
+num = 11
+name = "lakindu"
+print(num)
+print(name)
